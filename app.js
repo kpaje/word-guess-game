946e9c1386a5d8286191ff247f0e50bd202a9120
kpaje <kelvinmembership@gmail.com>
2020-02-03 00:28:32 -0800

    utilizing inwuirer to prompt the game

--- app.js
+++ app.js
@@ -1,75 +1,95 @@
 import wordGenerator from "./wordGenerator";
+import inquirer from "inquirer";
 
 let wins = 0;
 let losses = 0;
+let guess;
 let guessRemaining = 9;
 let guesses = [];
 const wordObject = wordGenerator.createWordObject();
 
 const objectAction = {
-	setMatchingBooleanValue: function(guess) {
-		for (let key in wordObject) {
-			if (key === guess) {
-				wordObject[key][1] = true; // Set wordObject Boolean
-			}
-		}
-	},
-	setMatchingGuessValue: function(guess) {
-		for (let key in wordObject) {
-			if (key === guess) {
-				wordObject[key][0] = guess; //set wordObject letter value
-			}
-		}
-	},
-	isAllBooleansTrue: function(object) {
-		let objectKeys = Object.keys(object);
-		let isAllTrue = objectKeys.every(key => {
-			let itemBoolean = object[key][1];
-			return itemBoolean;
-		});
+  setMatchingBooleanValue: function(guess) {
+    for (let key in wordObject) {
+      if (key === guess) {
+        wordObject[key][1] = true; // Set wordObject Boolean
+      }
+    }
+  },
+  setMatchingGuessValue: function(guess) {
+    for (let key in wordObject) {
+      if (key === guess) {
+        wordObject[key][0] = guess; //set wordObject letter value
+      }
+    }
+  },
+  isAllBooleansTrue: function(object) {
+    let objectKeys = Object.keys(object);
+    let isAllTrue = objectKeys.every(key => {
+      let itemBoolean = object[key][1];
+      return itemBoolean;
+    });
 
-		if (isAllTrue === true) {
-			console.log("YOU WIN");
-		}
-	}
+    if (isAllTrue === true) {
+      console.log("YOU WIN");
+      wins += 1;
+    }
+  }
 };
 
 const guessAction = {
-	addGuessInput: function(guess) {
-		guesses.push(guess);
-	},
-	subtractGuessesRemaining: function() {
-		guessRemaining = guessRemaining - 1;
-	}
+  addGuessInput: function(guess) {
+    guesses.push(guess);
+  },
+  subtractGuessesRemaining: function() {
+    guessRemaining -= 1;
+  }
 };
 
-const handlers = {
-	resolveGuessInput: function(guess) {
-		guessAction.subtractGuessesRemaining();
-		guessAction.addGuessInput(guess);
-	},
-	resolveObjects: function(guess) {
-		objectAction.setMatchingBooleanValue(guess);
-		objectAction.setMatchingGuessValue(guess);
-		objectAction.isAllBooleansTrue(wordObject);
-	},
-	resolveEntry: function(guess) {
-		handlers.resolveGuessInput(guess);
-		handlers.resolveObjects(guess);
-	},
-	gameOver: function() {
-		if (guessRemaining < 1) {
-			console.log("GAME OVER");
-		}
-	}
+const resolver = {
+  resolveGuessInput: function(guess) {
+    guessAction.subtractGuessesRemaining();
+    guessAction.addGuessInput(guess);
+  },
+  resolveObjects: function(guess) {
+    objectAction.setMatchingBooleanValue(guess);
+    objectAction.setMatchingGuessValue(guess);
+    objectAction.isAllBooleansTrue(wordObject);
+  },
+  resolveUserEntry: function(guess) {
+    resolver.resolveGuessInput(guess);
+    resolver.resolveObjects(guess);
+  },
+  gameOver: function() {
+    if (guessRemaining < 1) {
+      console.log("GAME OVER");
+      losses += 1;
+    }
+  }
 };
 
-handlers.resolveEntry("F");
-handlers.resolveEntry("U");
-// handlers.resolveEntry("B");
-handlers.resolveEntry("A");
-handlers.resolveEntry("R");
+inquirer
+  .prompt([
+    {
+      name: "inputGuess",
+      message: "Guess a Letter"
+    }
+  ])
+  .then(answers => {
+    guess = answers.inputGuess.toUpperCase();
+    console.info("Your Guess: ", guess);
+
+    resolver.resolveUserEntry(guess);
+    console.log("wordObject: ", wordObject);
+  });
+
+// resolver.resolveUserEntry("F");
+// resolver.resolveUserEntry("U");
+// resolver.resolveUserEntry("B");
+// resolver.resolveUserEntry("A");
+// resolver.resolveUserEntry("R");
 // objectAction.isAllBooleansTrue(wordObject);
-console.log("guessRemainingUPDATED: ", guessRemaining);
-console.log("wordObject: ", wordObject);
-console.log("guesses: ", guesses);
+// console.log("wins: ", wins);
+// console.log("losses: ", losses);
+// console.log("guessRemainingUPDATED: ", guessRemaining);
+// console.log("guesses: ", guesses);
--- wordGenerator.js
+++ wordGenerator.js
@@ -1,39 +1,39 @@
 const wordGenerator = {
-	generateWord: function() {
-		const wordList = [
-			"FUBAR"
-			// "SNAFU",
-			// "TARFU",
-			// "FUGAZI",
-			// "BCD",
-			// "LLMF",
-			// "MANPADS"
-		];
-		let randomNumber = Math.random() * wordList.length;
-		let randomInteger = Math.floor(randomNumber);
-		let genereatedWord = wordList[randomInteger];
-		return genereatedWord;
-	},
-	setWordToArray: function(word) {
-		return Array.from(word);
-	},
-	generateObjectFromArray: function(array) {
-		let object = {};
-		for (const key of array) {
-			object[key] = [" - ", false];
-		}
-		return object;
-	},
-	createWordObject: function() {
-		const newWord = wordGenerator.generateWord();
-		const wordArray = wordGenerator.setWordToArray(newWord);
-		const wordObject = wordGenerator.generateObjectFromArray(wordArray);
-
-		// console.log("newWord: ", newWord);
-		// console.log("wordArray: ", wordArray);
-		// console.log("wordObject: ", wordObject);
-		return wordObject;
-	}
+  generateWord: function() {
+    const wordList = [
+      "FUBAR"
+      // "SNAFU",
+      // "TARFU",
+      // "FUGAZI",
+      // "BCD",
+      // "LLMF",
+      // "MANPADS"
+    ];
+    let randomNumber = Math.random() * wordList.length;
+    let randomInteger = Math.floor(randomNumber);
+    let genereatedWord = wordList[randomInteger];
+    return genereatedWord;
+  },
+  setWordToArray: function(word) {
+    return Array.from(word);
+  },
+  generateObjectFromArray: function(array) {
+    let object = {};
+    for (const key of array) {
+      object[key] = [" - ", false];
+    }
+    return object;
+  },
+  createWordObject: function() {
+    const newWord = wordGenerator.generateWord();
+    const wordArray = wordGenerator.setWordToArray(newWord);
+    const wordObject = wordGenerator.generateObjectFromArray(wordArray);
+    return wordObject;
+  }
 };
 
 export default wordGenerator;
+
+// console.log("newWord: ", newWord);
+// console.log("wordArray: ", wordArray);
+// console.log("wordObject: ", wordObject);
