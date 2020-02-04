a8e3618262fb146bf3376b23e3ea02fb0514e0fa
kpaje <kelvinmembership@gmail.com>
2020-02-03 22:43:35 -0800

    moving to functional components. starting guess input component. custom formatted object in progress

---
+++ src/components/Guess.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const subtractGuessesRemaining = () => {
+  guessRemaining -= 1;
+};
+
+const addGuessInput = guess => {
+  guesses.push(guess);
+};
+
+export default function Guess() {
+  return <div></div>;
+}
--- src/components/wordGenerator.js
+++ src/components/wordGenerator.js
@@ -2,60 +2,63 @@ import React, { Component } from "react";
 import { TEST_OBJECT } from "../common/constants";
 import { WORD_LIST } from "../common/constants";
 
+const generateWord = () => {
+  const randomNumber = Math.random() * WORD_LIST.length;
+  const randomInteger = Math.floor(randomNumber);
+  const genereatedWord = WORD_LIST[randomInteger];
+  return genereatedWord;
+};
+const setWordToArray = word => {
+  return Array.from(word);
+};
+const generateObjectFromArray = array => {
+  let word = generateWord();
+  word = setWordToArray(word);
+
+  console.log(word);
+
+  const newArr = [];
+  const newObj = {
+    answer: "X",
+    hidden: "-",
+    reveal: false
+  };
+
+  newArr.push(newObj);
+  console.log(newArr);
+
+  return newObj;
+};
+const createWordObject = () => {
+  const newWord = generateWord();
+  const wordArray = setWordToArray(newWord);
+  const wordObject = generateObjectFromArray(wordArray);
+  return wordObject;
+};
+
+const renderObject = () => {
+  generateObjectFromArray();
+  return Object.entries(TEST_OBJECT).map(([key, value], index) => {
+    return (
+      <div key={key}>
+        <p>answer: {value.answer}</p>
+        <p>hidden: {value.hidden}</p>
+        <p>reveal: {String(value.reveal)}</p>
+        ---------
+      </div>
+    );
+  });
+};
+
 export default class WordGenerator extends Component {
-  generateWord() {
-    const randomNumber = Math.random() * WORD_LIST.length;
-    const randomInteger = Math.floor(randomNumber);
-    const genereatedWord = WORD_LIST[randomInteger];
-    return genereatedWord;
-  }
-  setWordToArray(word) {
-    return Array.from(word);
-  }
-  generateObjectFromArray(array) {
-    const object = {};
-    const arrayValue = [" - ", false];
-    for (const key of array) {
-      object[key] = arrayValue;
-    }
-    return object;
-  }
-  createWordObject() {
-    const newWord = this.generateWord();
-    const wordArray = this.setWordToArray(newWord);
-    const wordObject = this.generateObjectFromArray(wordArray);
-    return wordObject;
-  }
-
-  renderObject() {
-    const object = this.createWordObject();
-    Object.keys(object).map((key, i) => {
-      return <div i={i}>wordGenerator: {key}</div>;
-    });
-  }
-
-  // console.log("newWord: ", newWord);
-  // console.log("wordArray: ", wordArray);
-  // console.log("wordObject: ", wordObject);
-
   render() {
     return (
       <div>
         <div>
+          <div>Generated Word: {generateWord()}</div>
           <h2>wordGenerator</h2>
-          {Object.entries(TEST_OBJECT).map(([key, value], index) => {
-            return (
-              <div key={key}>
-                <p>answer: {value.answer}</p>
-                <p>hidden: {value.hidden}</p>
-                <p>reveal: {String(value.reveal)}</p>
-                ---------
-              </div>
-            );
-          })}
+          {renderObject()}
         </div>
-
-        <div>Generated Word: {this.generateWord()}</div>
       </div>
     );
   }
