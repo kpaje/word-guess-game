b5a2e45696f50bd9491173db7a141293ab7390b1
kpaje <kelvinmembership@gmail.com>
2020-02-03 16:09:29 -0800

    migrating to create-react-app, other boiler was too opinionated

--- src/app.js
+++ src/app.js
@@ -1,90 +1,26 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable guard-for-in */
-/* eslint-disable no-console */
-/* eslint-disable no-restricted-syntax */
-const wordGenerator = require('./wordGenerator');
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-let wins = 0;
-let losses = 0;
-let guessRemaining = 9;
-const guesses = [];
-const wordObject = wordGenerator.createWordObject();
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-const objectAction = {
-  setMatchingBooleanValue(guess) {
-    for (const key in wordObject) {
-      if (key === guess) {
-        wordObject[key][1] = true; // Set wordObject Boolean
-      }
-    }
-  },
-  setMatchingGuessValue(guess) {
-    for (const key in wordObject) {
-      if (key === guess) {
-        wordObject[key][0] = guess; // set wordObject letter value
-      }
-    }
-  },
-  setGameWord() {
-    for (const key in wordObject) {
-      const blankKey = wordObject[key][0];
-      console.log([blankKey]); // set wordObject to mystery values
-    }
-  },
-  isAllBooleansTrue(object) {
-    const objectKeys = Object.keys(object);
-    const isAllTrue = objectKeys.every(key => {
-      const itemBoolean = object[key][1];
-      return itemBoolean;
-    });
-
-    if (isAllTrue === true) {
-      console.log('YOU WIN');
-      wins += 1;
-    }
-  },
-};
-
-const guessAction = {
-  addGuessInput(guess) {
-    guesses.push(guess);
-  },
-  subtractGuessesRemaining() {
-    guessRemaining -= 1;
-  },
-};
-
-const resolver = {
-  resolveGuessInput(guess) {
-    guessAction.subtractGuessesRemaining();
-    guessAction.addGuessInput(guess);
-  },
-  resolveObjects(guess) {
-    objectAction.setMatchingBooleanValue(guess);
-    objectAction.setMatchingGuessValue(guess);
-    objectAction.isAllBooleansTrue(wordObject);
-  },
-  resolveUserEntry(guess) {
-    resolver.resolveGuessInput(guess);
-    resolver.resolveObjects(guess);
-  },
-  gameOver() {
-    if (guessRemaining < 1) {
-      console.log('GAME OVER');
-      losses += 1;
-    }
-  },
-};
-
-objectAction.setGameWord();
-
-// resolver.resolveUserEntry("F");
-// resolver.resolveUserEntry("U");
-// resolver.resolveUserEntry("B");
-// resolver.resolveUserEntry("A");
-// resolver.resolveUserEntry("R");
-// objectAction.isAllBooleansTrue(wordObject);
-// console.log("wins: ", wins);
-// console.log("losses: ", losses);
-// console.log("guessRemainingUPDATED: ", guessRemaining);
-// console.log("guesses: ", guesses);
+export default App;
