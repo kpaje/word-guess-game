1ce591d46e80ce48bac710a36806fae604175cdf
kpaje <kelvinmembership@gmail.com>
2020-02-04 17:49:14 -0800

    added hooks for rendering keypress and keyArray

--- src/components/Guess.js
+++ src/components/Guess.js
@@ -2,56 +2,20 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 export default function Guess() {
-  let keysPressed = [];
-  const happyPress = useKeyPress("a");
-  const sadPress = useKeyPress("s");
-  const robotPress = useKeyPress("d");
-  const foxPress = useKeyPress("f");
+  const [keyValue, setkeyValue] = useState("");
+  const [keyArray, setkeyArray] = useState([]);
 
-  //   function useKey(key) {
-  //     // Keep track of key state
-  //     const [pressed, setPressed] = useState(false)
-
-  //     // Does an event match the key we're watching?
-  //     const match = event => key.toLowerCase() == event.key.toLowerCase()
-
-  //     // Event handlers
-  //     const onDown = event => {
-  //         if (match(event)) setPressed(true)
-  //     }
-
-  //     const onUp = event => {
-  //         if (match(event)) setPressed(false)
-  //     }
-
-  //     // Bind and unbind events
-  //     useEffect(() => {
-  //         window.addEventListener("keydown", onDown)
-  //         window.addEventListener("keyup", onUp)
-  //         return () => {
-  //             window.removeEventListener("keydown", onDown)
-  //             window.removeEventListener("keyup", onUp)
-  //         }
-  //     }, [key])
-
-  //     return pressed
-  // }
-
-  function useKeyPress(targetKey) {
-    const [keyPressed, setKeyPressed] = useState(false);
-
+  const useKeyPress = () => {
     const keyDownHandler = ({ key }) => {
-      if (key === targetKey) {
-        setKeyPressed(true);
-      }
+      key = key.toUpperCase();
+      setkeyValue(key);
+      setkeyArray(key);
+      keyArray.push(key);
+      setkeyArray(keyArray);
     };
 
     const upHandler = ({ key }) => {
-      if (key === targetKey) {
-        setKeyPressed(false);
-        keysPressed.push(key);
-        console.log(keysPressed);
-      }
+      console.log(keyArray);
     };
 
     useEffect(() => {
@@ -63,20 +27,13 @@ export default function Guess() {
         window.removeEventListener("keyup", upHandler);
       };
     }, []); // Empty array ensures that effect is only run on mount and unmount
-
-    return keyPressed;
-  }
+  };
 
   return (
     <div>
-      <div>a, s, d, f</div>
-      <div>{String(keysPressed)}</div>
-      <div>
-        {happyPress && "😊"}
-        {sadPress && "😢"}
-        {robotPress && "🤖"}
-        {foxPress && "🦊"}
-      </div>
+      {useKeyPress()}
+      <h2>keyPressed: {keyValue}</h2>
+      <h2>KeyArray: {keyArray}</h2>
     </div>
   );
 }
