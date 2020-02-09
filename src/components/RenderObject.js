25b1180d917c13f02bc32528e47c7d0b1497a9fe
kpaje <kelvinmembership@gmail.com>
2020-02-09 00:30:41 -0800

    went back from destructuring. Cannot iterate through value

--- src/components/RenderObject.js
+++ src/components/RenderObject.js
@@ -2,12 +2,11 @@ import React from "react";
 
 export default function RenderObject({ object }) {
   return Object.entries(object).map(([key, value]) => {
-    const [answer, hidden, reveal] = value;
     return (
       <tr key={key}>
-        <td>{answer}</td>
-        <td>{hidden}</td>
-        <td>{String(reveal)}</td>
+        <td>{value.answer}</td>
+        <td>{value.hidden}</td>
+        <td>{String(value.reveal)}</td>
       </tr>
     );
   });
